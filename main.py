8470aaaa5abd9d65cf4b79d3f50701e4713d4ad2
Aares <111aares123@gmail.com>
2026-06-26 11:13:37 +0530

    Added database functions

--- main.py
+++ main.py
@@ -1,14 +1,6 @@
-import sqlite3
-def connect():
-    conn = sqlite3.connect("grades.db")
-    return conn
-def create_table():
-    conn=connect()
-    cursor=conn.cursor()
-    cursor.execute("""CREATE TABLE IF NOT EXISTS students(id integer primary key autoincrement, name text, math integer,
-                    science integer, english integer)""")
-    conn.commit()
-    conn.close()
-    print("Table ready!")
-
+from database import create_table, add_student
+if choice == "1":
+    add_student()
 create_table()
+print("Welcome to Student Grade Manager!")
+
