183b97309a969d3aa54d9bcd5b13de897df70663
Aares <111aares123@gmail.com>
2026-06-26 12:46:00 +0530

    retouched main.py

--- main.py
+++ main.py
@@ -1,25 +1,23 @@
-from database import create_table, add_student
+from database import (create_table, add_student, view_students,
+                      search_student, update_student, delete_student)
 from menu import display_menu
-create_table()
-while True:
-    choice=display_menu()
 
-    if choice =="1":
-        add_student()
-    elif choice == "2":
-            from database import view_students
+if __name__ == "__main__":
+    create_table()
+
+    while True:
+        choice = display_menu()
+
+        if choice == "1":
+            add_student()
+        elif choice == "2":
             view_students()
-    elif choice == "3":
-         from database import search_student
-         search_student()
-    elif choice == "4":
-        from database import update_student
-        update_student()
-    elif choice == "5":
-        from database import delete_student
-        delete_student()  
-    elif choice == "6":
-        print("Goodbye!")
-        break
-
-
+        elif choice == "3":
+            search_student()
+        elif choice == "4":
+            update_student()
+        elif choice == "5":
+            delete_student()
+        elif choice == "6":
+            print("Goodbye!")
+            break
