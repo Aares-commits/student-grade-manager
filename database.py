8ba7e940535b2b5abf90ab396ceb887168beddb8
Aares <111aares123@gmail.com>
2026-06-26 11:57:19 +0530

    added view students function in database

--- database.py
+++ database.py
@@ -25,3 +25,24 @@ def add_student():
     conn.commit()
     conn.close()
     print("Student added succesfully!")
+def view_students():
+    conn=connect()
+    cursor=conn.cursor()
+    cursor.execute("SELECT * FROM students")
+    cursor.fetchall()
+    students=cursor.fetchall()
+    if not students:
+        print("No students found.")
+    else:
+        for student in students:
+            print(f"ID: {student[0]}")
+            print(f"Name: {student[1]}")
+            print(f"Math: {student[2]}")
+            print(f"Science: {student[3]}")
+            print(f"English: {student[4]}")
+            print("-" * 30)
+    conn.commit()
+    conn.close()
+    print("Students views succesfully")
+    
+    
