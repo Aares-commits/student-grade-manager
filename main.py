ed77c956921465bbffa735e3336bddde3cf858f7
Mithran <mithranraghland02@gmail.con>
2026-06-26 12:10:17 +0530

    git push origin mai

--- main.py
+++ main.py
@@ -6,6 +6,18 @@ while True:
 
     if choice =="1":
         add_student()
+    elif choice == "2":
+            from database import view_students
+            view_students()
+    elif choice == "3":
+         from database import search_student
+         search_student()
+    elif choice == "4":
+        from database import update_student
+        update_student()
+    elif choice == "5":
+        from database import delete_student
+        delete_student()  
     elif choice == "6":
         print("Goodbye!")
         break
